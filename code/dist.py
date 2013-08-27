baf3c5fb73ff8ebd536e1310e334892f37b682c5
Matias Estrada <tonyhawz@gmail.com>
2013-08-26 22:44:13 -0300

    test

--- code/dist.py
+++ code/dist.py
@@ -7,10 +7,10 @@ butia = usb4butia.USB4Butia()
 # imprimo distancia
 while True:
     butia.refresh()
-    dist= butia.getDistance(2)
-    dist1= butia.getDistance(5)
-    dist2= butia.getDistance(6)
-    print dist
-    print dist1
-    print dist2
+    dist = butia.getDistance(2)
+    dist1 = butia.getDistance(5)
+    dist2 = butia.getDistance(6)
+    print(dist)
+    print(dist1)
+    print(dist2)
     time.sleep(1)
